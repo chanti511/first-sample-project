92a0429954dfd7b54b43769fd3f9c655a530e4f0
chanti511 <98020669+chanti511@users.noreply.github.com>
2022-02-02 07:08:49 +0530

    Update App.java

--- src/main/java/com/demosample/first_sample_project/App.java
+++ src/main/java/com/demosample/first_sample_project/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "This is Sample java Progaram to test the data!" );
     }
 }
